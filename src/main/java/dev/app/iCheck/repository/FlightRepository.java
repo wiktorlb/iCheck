ee4b98e74dcca5af0085b4ba015eaac36ba754bf
Wiktor <wiktorlabocha123@gmail.com>
2025-01-15 00:54:52 +0100

    FlightBoard + AddNewFlight

--- src/main/java/dev/app/iCheck/repository/FlightRepository.java
+++ src/main/java/dev/app/iCheck/repository/FlightRepository.java
@@ -1,9 +1,15 @@
 package dev.app.iCheck.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 import dev.app.iCheck.model.Flight;
 
+@Repository
 public interface FlightRepository extends MongoRepository<Flight, String> {
+    List<Flight> findByDepartureDate(String departureDate);
 }
 
 // package dev.app.iCheck.repository;
